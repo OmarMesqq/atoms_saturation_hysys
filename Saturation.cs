3b14cf4600d9007cd664fd1d5a768f73a7210c30
OmarMesqq <omarmsqt@gmail.com>
2023-11-09 21:37:29 -0300

    refactor: :fire: remove some unnecessary in the actual implementation

--- Saturation.cs
+++ Saturation.cs
@@ -22,18 +22,9 @@ namespace Atoms
 
     public class Saturation
     {
-        [DllImport("dummy_kernel.dll")]
-        public static extern int send_scalar_t();
-        [DllImport("dummy_kernel.dll")]
-        public static extern float send_scalar_p();
-        [DllImport("dummy_kernel.dll")]
-        public static extern VectorY send_vector_y();
-
-        [DllImport("dummy_kernel.dll")]
+        [DllImport("atoms_saturation_kernel.dll")]
         public static extern void info_dump(IntPtr str);
 
-        [DllImport("dummy_kernel.dll")]
-        public static extern void receive_scalar_yw(float number);
 
         private void LogInfo(string message)
         {
@@ -78,8 +69,8 @@ namespace Atoms
         {
             try
             {
-                if (isForgetpass == true) {return;}
-                
+                if (isForgetpass == true) { LogInfo("isForgetpass is true"); return; }
+
                 Feed = myContainer.FindVariable("FeedStream").Variable.Object;
                 Product = myContainer.FindVariable("ProductStream").Variable.Object;
                 eosModel = myContainer.FindVariable("Eos_Model").Variable;
@@ -90,15 +81,15 @@ namespace Atoms
 
 
                 if (Feed == null) { LogInfo("Feed stream is null"); return; }
-                
+
                 if (Product == null) { LogInfo("Product stream is null"); return; }
-                
+
                 if (eosModel.Value == emptyVal) { LogInfo("EoS model is empty"); return; }
-                
+
                 if (assocModel.Value == emptyVal) { LogInfo("Assoc model is empty"); return; }
                 if (waterFraction.Value == emptyVal) { LogInfo("Water frac is empty"); return; }
                 if (crossH2S.Value == emptyVal) { LogInfo("Cross H2S is empty"); return; }
-                if (crossCO2.Value == emptyVal) { LogInfo("Cross CO2"); return; }
+                if (crossCO2.Value == emptyVal) { LogInfo("Cross CO2 is empty"); return; }
 
 
                 LogInfo("EoS Model:");
@@ -111,43 +102,6 @@ namespace Atoms
                 LogInfo(crossH2S.Value.ToString());
                 LogInfo("Cross CO2 model:");
                 LogInfo(crossCO2.Value.ToString());
-
-
-                LogInfo("----- Rust/C# dummy bindings below: -----");
-                int t = send_scalar_t();
-                float p = send_scalar_p();
-                VectorY y = send_vector_y();
-
-                LogInfo("t:");
-                LogInfo(t.ToString());
-
-                LogInfo("p:");
-                LogInfo(p.ToString());
-
-                double[] data = new double[y.size];
-                Marshal.Copy(y.data, data,0, y.size);
-                LogInfo("Value of the vector Y is:");
-                for (int i = 0; i < y.size; i++)
-                {
-                    LogInfo(data[i].ToString());
-                }
-
-                LogInfo("Size of the vector Y is:");
-                LogInfo(y.size.ToString());
-
-                float myNum = 2525353.53f;
-                receive_scalar_yw(myNum);
-            }
-            catch {}
-        }
-
-        public void StatusQuery(ObjectStatus Status)
-        {
-            try
-            {
-                if (myContainer.ExtensionInterface.IsIgnored == true) { return; }
-                if (Feed == null) { Status.AddStatusCondition(HYSYS.StatusLevel_enum.slMissingRequiredInformation, 1, "Requires an Inlet Stream"); }
-                if (Product == null) { Status.AddStatusCondition(HYSYS.StatusLevel_enum.slMissingRequiredInformation, 2, "Requires an Outlet Stream"); }
             }
             catch { }
         }
